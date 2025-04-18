fb3b90d497f8f4577ee7c2b32d7ff71b046750e3
David Williams <david.williams@digital.trade.gov.uk>
2025-04-18 19:56:17 +0100

    Final pylint fixes

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -98,6 +98,7 @@ def registration(request):
 # def get_dealerships(request):
 # Update the `get_dealerships` render list of dealerships all by default, particular state if state is passed
 def get_dealerships(request, state="All"):
+    print(request)
     if state == "All":
         endpoint = "/fetchDealers"
     else:
